d5c673303afdff6b840d0f08db97eb187fb9521a
Якубчик Степан <stepanyakubchik@yandex.ru>
2024-12-05 00:08:50 +0500

    module9-task1

--- js/util.js
+++ js/util.js
@@ -2,4 +2,6 @@ function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export {getRandomInt};
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInt, isEscapeKey};
