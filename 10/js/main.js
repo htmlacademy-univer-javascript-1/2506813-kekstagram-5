8546f0b22036f567917760aa5d7211ecc4843e2c
Keks <keks@htmlacademy.ru>
2024-12-04 22:10:26 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,4 @@
+import {generatePhotos} from './data.js';
+import {renderGallery} from './gallery.js';
+import './form.js';
+renderGallery(generatePhotos());
---
+++ 10/js/util.js
@@ -0,0 +1,7 @@
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInt, isEscapeKey};
