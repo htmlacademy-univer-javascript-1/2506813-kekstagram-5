f789fa7bcdc6de41a34e5133da33d6e0bf5191e4
Якубчик <72699381+Stepadze@users.noreply.github.com>
2025-01-03 02:49:04 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 import { renderGallery } from './gallery.js';
 import { getData, sendData } from './api.js';
 import { showAlert, debounce } from './util.js';
@@ -24,45 +23,3 @@ try {
 } catch (err) {
   showAlert(err.message);
 }
-
-import { createPictures } from './preview.js';
-import './open-post.js';
-import './hashtag-pristine.js';
-import { openForm } from './form.js';
-import { loadData } from './api.js';
-import { initFilters } from './filters.js';
-
-let pictures = [];
-
-const onSuccess = (data) => {
-  pictures = data.slice();
-  createPictures(pictures);
-  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
-
-};
-
-const onFail = () =>{
-  const errorMesage = document.createElement('div');
-  errorMesage.style.position = 'absolute';
-  errorMesage.style.left = 0;
-  errorMesage.style.top = 0;
-  errorMesage.style.right = 0;
-
-  errorMesage.style.fontSize = '20px';
-  errorMesage.style.backgroundColor = '#e1375f';
-  errorMesage.style.padding = '15px';
-
-  errorMesage.style.textAlign = 'center';
-  errorMesage.textContent = 'Ошибка при загрузке изображений';
-  document.body.append(errorMesage);
-
-};
-
-
-loadData(onSuccess, onFail);
-openForm();
-
-initFilters();
-
-export {pictures};
-
